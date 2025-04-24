08ecf8e827d7e05c0457b3b8b8b3ba42a9507d8e
Tarang <tarang.teredesai@wsu.edu>
2025-04-23 22:55:34 -0700

    base charecter

--- CPTS122-PA9/_libs.hpp
+++ CPTS122-PA9/_libs.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+typedef unsigned int uint;
+
 using std::cin; // cin >> 
 using std::cout; // cout <<
 using std::endl;
@@ -11,6 +13,15 @@ using std::cerr; //error output
 using std::string;
 using std::to_string; //for the time stuff
 
+#define SCREEN_HEIGHT 720 
+#define SCREEN_WIDTH  1280
+
+
+//SMFL libs
+#include <SFML/Graphics.hpp>
+
+
+
 #define RST "\033[0;0m"
 #define RED "\033[0;31m"
 #define GRN "\033[0;32m"
@@ -18,6 +29,3 @@ using std::to_string; //for the time stuff
 #define BLU "\033[0;34m"
 #define MAG "\033[0;35m"
 #define CYN "\033[0;36m"
-
-#define SCREEN_HEIGHT 720 
-#define SCREEN_WIDTH  1280
